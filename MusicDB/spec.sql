fcad0abbc2928114604acc4afb8967e4a648da61
bgoldfischer <bgoldfischer@cpucodeschool.com>
2022-01-06 22:25:18 +0000

    partial work

---
+++ MusicDB/spec.sql
@@ -0,0 +1,26 @@
+/*
+Rentals
+    primary key
+    CustomerName
+    InstrumentName
+    InstrumentId (3 letters of name and 3 letters of cat)
+    Category
+    MonthlyRentalFee
+    DateRented
+    DateReturned
+    TotalProfit
+    DateInserted datetime
+
+    min-1
+    max-12
+    
+
+
+
+
+
+
+
+
+
+*/
